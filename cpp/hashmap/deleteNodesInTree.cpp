24f89cde9d4b4acdfb5c90ee67d162f6befdeb1a
SalonikResch <Salonik.Resch@gmail.com>
2024-01-14 16:54:28 -0800

    unordered_map and trees

---
+++ cpp/hashmap/deleteNodesInTree.cpp
@@ -0,0 +1,78 @@
+// Delete nodes in a tree to make a forest
+// Uses unordered_map to efficiently search for nodes to delete
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+ #include <unordered_map>
+ typedef unordered_map<int, bool> hashmap;
+ typedef unordered_map<TreeNode*, bool> treemap;
+
+void search(TreeNode* root, treemap& inRoots, hashmap& toDelete, TreeNode *parent){
+    if(root == NULL)
+        return;
+    // Check if this node needs to be deleted
+    if(toDelete.find(root->val) != toDelete.end()){
+        // Add children to roots
+        if(root->left != NULL)
+            inRoots.insert({root->left, true});
+        if(root->right != NULL)
+            inRoots.insert({root->right, true});
+
+        // Remove root from roots, if already a root
+        if(inRoots.find(root) != inRoots.end())
+            inRoots[root] = false;
+
+        // Trim the tree
+        if(parent != NULL){
+            if(parent->left == root){
+                parent->left = NULL;
+            }else{
+                parent->right = NULL;
+            }
+        }
+    }
+    search(root->left, inRoots, toDelete, root);
+    search(root->right, inRoots, toDelete, root);
+}
+
+
+class Solution {
+public:
+    vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete) {
+        // Vector of roots
+        vector<TreeNode*> roots;
+        // Hashmap of root
+        treemap inRoots;
+        // Hashmap of deletes
+        hashmap toDelete;
+        // Fill hashmap of deletes
+        for(int i=0;i<to_delete.size();i++){
+            toDelete.insert({to_delete[i], true});
+        }
+
+        if(toDelete.find(root->val) == toDelete.end()){
+            inRoots[root] = true;
+        }
+        
+        // Search the tree
+        search(root, inRoots, toDelete, NULL);
+
+        // Add roots to vector
+        for(auto it : inRoots){
+            if(it.second)
+                roots.push_back(it.first);
+        }
+        return roots;
+    }
+};
